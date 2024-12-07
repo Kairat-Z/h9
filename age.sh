0bd2aaa950833ffd9e9341d1b23f596b52080686
EC2 Default User <ec2-user@ip-172-31-86-92.ec2.internal>
2024-12-07 00:00:23 +0000

    updated -n

--- age.sh
+++ age.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -n 4 -p "Please input your year of birth: " yob
+read -p "Please input your year of birth: " yob
 echo  you are $((2024-$yob))years old
 echo you were $((2020-$yob)) years old in 2020
 echo you will be $((2030-$yob)) years in 2030
